d55f4b15186668f75433349a8350eb0a57ee46a5
0chen <admin@hi-ourlife.com>
2021-09-10 15:13:40 +0800

    update

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ import os
 
 USER_INFO = os.getenv('USER_INFO')
 PUSH_KEY = os.getenv('PUSH_KEY')
-secret = os.getenv('Secret')
-webhook = os.getenv('Webhook')
+secret = os.getenv('SECRET')
+webhook = os.getenv('WEBHOOK')
 
 USER_INFO_ARR = USER_INFO.split("|", 1)
 USERNAME = USER_INFO_ARR[0]
